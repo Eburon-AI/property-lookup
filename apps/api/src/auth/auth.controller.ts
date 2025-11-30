3e725fbebea9570939f394cf86004a893cd67dcc
Oleksandr Ptukha <oleksandrptuha1@gmail.com>
2025-11-30 13:47:02 +0100

    feat:initialized Nest app in api folder

---
+++ apps/api/src/auth/auth.controller.ts
@@ -0,0 +1,12 @@
+import { AuthService } from './auth.service';
+import { LoginDto } from './dtos/login.dto';
+import { Body, Controller, Post } from '@nestjs/common';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+  @Post('login')
+  login(@Body() loginDto: LoginDto) {
+    return this.authService.login(loginDto);
+  }
+}
